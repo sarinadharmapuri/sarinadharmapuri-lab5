d02a0294da4f0fc039ae9b05c038ea1b1b3be260
sarinadharmapuri <sarina.dharmapuri@gwmail.gwu.edu>
2026-03-03 13:38:45 -0500

    adding changes to encrypt/decrypt methods in Enigma

--- Enigma.java
+++ Enigma.java
@@ -20,13 +20,53 @@ public class Enigma{
 
 
     public String decrypt(String message){        
-        //TODO
+        //Find the character on the outer rotor
+        String result = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            char current = message.charAt(i);
+            int outerIdx = rotors[2].indexOf(current);
+
+            char middleLetter = rotors[1].charAt(outerIdx);
+            int outerIdx2 = rotors[2].indexOf(middleLetter);
+            char finalLetter = rotors[0].charAt(outerIdx2);
+
+            result += finalLetter;
+
+            rotate();
+
+        }
+
+        return result;
+        // note the character aligned with it on the middle rotor
+        // find that character on the outer rotor 
+        // then output the character aligned with it on the inner rotor. 
     }
 
 
     
     public String encrypt(String message){
-        //TODO
+        // find current letter on innermost ring
+        String result = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            char current = message.charAt(i);
+            char outerLetter = rotors[2].charAt(i);
+
+            int middleIdx = rotors[1].indexOf(outerLetter);
+            char finalLetter = rotors[2].charAt(middleIdx);
+
+            result += finalLetter;
+
+            rotate();
+
+
+        }
+
+        return result;
+        // find letter on outer ring above current letter
+        // find letter on outer ring in the middle ring
+        // find letter above letter in the middle ring
     }
 
     
